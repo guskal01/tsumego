0559e6ea68dcbda2cfa0703acf488f27040c564e
Gustav <gustav.kalander@hotmail.se>
2022-06-03 16:33:03 +0200

    distutils -> setuptools

--- tsumegoboard/compile.sh
+++ tsumegoboard/compile.sh
@@ -1,2 +1,2 @@
 python3 setup.py build
-cp -a build/lib.linux-x86_64-3.8/. ../
+cp -a build/lib.linux-x86_64-3.10/. ../
--- tsumegoboard/setup.py
+++ tsumegoboard/setup.py
@@ -1,3 +1,3 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 setup(name="tsumegoboard", version="1.0", ext_modules=[Extension("tsumegoboard", ["tsumegoboard.cpp"])])
